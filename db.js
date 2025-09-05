9205402bd40a72b9e7eb9a003e20247dec742b44
Tumaki88 <adityagiri1001001@gmail.com>
2025-09-05 14:17:36 +0530

    Connect backend to Filess.io MariaDB using env variables

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ import mysql from "mysql2/promise";
 
 // Create a pool using environment variables
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "yourpassword",
-  database: process.env.DB_NAME || "planvo",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
